dde602133f1c3f55f1f4b6b0ce50514922ec0490
AlvinAbiluse <alvinabiliuse@gmail.com>
2025-01-13 23:14:59 +0400

    completed ball transition

--- transition/ball.js
+++ transition/ball.js
@@ -1,1 +1,11 @@
+
+const bodyEl = document.querySelector("html");
+const ballEl = document.querySelector(".ball");
+
+bodyEl.addEventListener("click", (e) => {
+	ballEl.style.transform = 
+		`translateX(${e.clientX - 50}px) 
+		 translateY(${e.clientY - 50}px)`;
+	ballEl.style.margin = "0px";
+});
 	
