8528a4db7e8d3f638b9ed9fefa4fca4cf7d30d70
AlvinAbiluse <alvinabiliuse@gmail.com>
2025-01-13 22:44:46 +0400

    transitions

--- transition/script.js
+++ transition/script.js
@@ -22,11 +22,11 @@ const third = document.querySelector(".third");
 third.addEventListener("click", (e) => {
 	const point = 
 		`translateX(${e.clientY - 25}px translau${e.clientX - 25}px)`
-	console.log(e.clientX);
-	console.log(e.clientY);
+	console.log(e.clientX + "\n" + e.screenX + "\n\n");
+	console.log(e.clientY + "\n" + e.screenY + "\n\n");
 	console.log(e);
 	document.querySelector(".ball").style.transform = 
-		`translateX(${e.clientX - 25}px`;
+		`translateX(${e.pageX - 25}px`;
 	document.querySelector(".ball").style.transform+= 
-		`translateY(${e.clientY - 25}px`;
+		`translateY(${e.pageY - 25}px`;
 });
