5c9e80ab555c143b42093be998abc16fe8028f87
AlvinAbiluse <alvinabiliuse@gmail.com>
2025-01-13 22:58:16 +0400

    removed third card from js and edited it from css

--- transition/script.js
+++ transition/script.js
@@ -13,20 +13,3 @@ function showing() {
 			"show");
 }
 
-
-
-
-
-const third = document.querySelector(".third");
-
-third.addEventListener("click", (e) => {
-	const point = 
-		`translateX(${e.clientY - 25}px translau${e.clientX - 25}px)`
-	console.log(e.clientX + "\n" + e.screenX + "\n\n");
-	console.log(e.clientY + "\n" + e.screenY + "\n\n");
-	console.log(e);
-	document.querySelector(".ball").style.transform = 
-		`translateX(${e.pageX - 25}px`;
-	document.querySelector(".ball").style.transform+= 
-		`translateY(${e.pageY - 25}px`;
-});
