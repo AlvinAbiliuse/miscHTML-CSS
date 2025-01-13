4fb7376432f208a53c5b5f755ede09d053e80b7a
AlvinAbiluse <alvinabiliuse@gmail.com>
2025-01-13 22:33:31 +0400

    working on third transition

--- transition/script.js
+++ transition/script.js
@@ -12,3 +12,21 @@ function showing() {
 	document.querySelector(".second div").classList.toggle(
 			"show");
 }
+
+
+
+
+
+const third = document.querySelector(".third");
+
+third.addEventListener("click", (e) => {
+	const point = 
+		`translateX(${e.clientY - 25}px translau${e.clientX - 25}px)`
+	console.log(e.clientX);
+	console.log(e.clientY);
+	console.log(e);
+	document.querySelector(".ball").style.transform = 
+		`translateX(${e.clientX - 25}px`;
+	document.querySelector(".ball").style.transform+= 
+		`translateY(${e.clientY - 25}px`;
+});
